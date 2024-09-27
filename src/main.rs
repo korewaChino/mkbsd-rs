7d3253a8203eb7d950ccef11ac05a5b8a266d4fc
Cappy Ishihara <cappy@cappuchino.xyz>
2024-09-27 13:43:43 +0700

    relicense to unlicense

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,6 @@
 //!
 //! There seems to be a standardized layout for the images, so we are going to iterate through all the repos and rip everything
 
-/*
-    This program is free software. It comes without any warranty, to
-    the extent permitted by applicable law. You can redistribute it
-    and/or modify it under the terms of the Do What The Fuck You Want
-    To Public License, Version 2, as published by Sam Hocevar. See
-    http://www.wtfpl.net/ for more details.
-*/
-
 use clap::{Parser, ValueEnum};
 
 mod simple;
